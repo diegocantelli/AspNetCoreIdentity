745718ac6d1d832edfe1424624fb0dd7c6a9f9cf
Diego Cantelli <diegocantelli19@gmail.com>
2022-01-22 13:34:11 -0300

    Criando uma policy para que apenas o departamento de recursos humanos tenha acesso à página de recursos humanos

--- IdentityUnderTheHood/Pages/Account/Login.cshtml.cs
+++ IdentityUnderTheHood/Pages/Account/Login.cshtml.cs
@@ -28,7 +28,10 @@ namespace IdentityUnderTheHood.Pages.Account
                 var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, "admin"),
-                    new Claim(ClaimTypes.Email, "admin@admin.com")
+                    new Claim(ClaimTypes.Email, "admin@admin.com"),
+
+                    //Com esta claim será possível acessar a página de HumanResource
+                    new Claim("Department", "HR")
                 };
 
                 var identity = new ClaimsIdentity(claims, "MyCookieAuth");
