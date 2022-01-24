258bbc74c312bfbf9628ab8ed58a2f924818a59a
Diego Cantelli <diegocantelli19@gmail.com>
2022-01-24 19:09:19 -0300

    Persistindo o token em um cookie e obtendo o cookie com o token para autorizar nos endpoints

--- IdentityUnderTheHood/Pages/HRmanager.cshtml.cs
+++ IdentityUnderTheHood/Pages/HRmanager.cshtml.cs
@@ -2,9 +2,11 @@ using IdentityUnderTheHood.Authorization;
 using IdentityUnderTheHood.DTO;
 using IdentityUnderTheHood.Pages.Account;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -26,13 +28,40 @@ namespace IdentityUnderTheHood.Pages
         }
         public async Task OnGetAsync()
         {
+            WeatherForecastItens = await InvokeEndpoint<List<WeatherForecastDTO>>("OurWebApi", "WeatherForecast");
+        }
+
+        private async Task<JwtToken> Authenticate()
+        {
             var client = _httpClientFactory.CreateClient("OurWebApi");
             var res = await client.PostAsJsonAsync("auth", new Credential { UserName = "Admin", Password = "123" });
+            res.EnsureSuccessStatusCode();
             var strJwt = await res.Content.ReadAsStringAsync();
-            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
 
+            HttpContext.Session.SetString("access_token", strJwt);
+            return JsonConvert.DeserializeObject<JwtToken>(strJwt);
+        }
+
+        private async Task<T> InvokeEndpoint<T>(string clientName, string url)
+        {
+            JwtToken token = null;
+            var strTokenObj = HttpContext.Session.GetString("access_token");
+
+            if (string.IsNullOrEmpty(strTokenObj))
+            {
+                token = await Authenticate();
+            }
+            else
+                token = JsonConvert.DeserializeObject<JwtToken>(strTokenObj);
+
+            if (token == null || string.IsNullOrWhiteSpace(token.AccessToken) || token.ExpiresAt <= DateTime.UtcNow)
+            {
+                token = await Authenticate();
+            }
+
+            var client = _httpClientFactory.CreateClient(clientName);
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
-            WeatherForecastItens = await client.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast");
+            return await client.GetFromJsonAsync<T>(url);
         }
     }
 }
