0b1c1eee1a2a9058a664998271a5199dc706b8aa
Diego Cantelli <diegocantelli19@gmail.com>
2022-01-25 17:34:11 -0300

    Criando um usuário na base usando o UserManager do Identity

--- WebApp.Identity/Pages/Account/Register.cshtml.cs
+++ WebApp.Identity/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,10 +11,43 @@ namespace WebApp.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         [BindProperty]
         public RegisterViewModel RegisterViewModel { get; set; }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = new IdentityUser
+            {
+                Email = RegisterViewModel.Email,
+                UserName = RegisterViewModel.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, RegisterViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return Page();
+            }
         }
     }
 
