c84c4de425564e4725210b13acfa8961a4734e3e
Diego Cantelli <diegocantelli19@gmail.com>
2022-01-25 10:04:54 -0300

    COnfigurando a conexão com a base e aplicando as migrações para criar a base do identity

--- WebApp.Identity/Startup.cs
+++ WebApp.Identity/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Identity.Data;
 
 namespace WebApp.Identity
 {
@@ -23,6 +25,10 @@ namespace WebApp.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
             services.AddRazorPages();
         }
 
